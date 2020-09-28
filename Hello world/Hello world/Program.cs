ae1152e815c7b9a79d8a3c1274fd6eff0cbcf5b8
tronghieu271220 <72005037+tronghieu271220@users.noreply.github.com>
2020-09-28 13:56:52 +0700

    Add files via upload

---
+++ Hello world/Hello world/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" Hello");
+            Console.ReadKey();
+        }
+    }
+}
